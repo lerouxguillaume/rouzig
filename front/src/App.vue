1d58dbc2e6e06ae933e9f539a9b4a3f885a3ed09
Guillaume Le Roux <guillaume.le.roux@hotmail.fr>
2020-05-24 20:21:30 +0100

    add footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,16 +4,19 @@
     <div class="page-container">
       <router-view></router-view>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 
   import Header from "./components/Header/Header";
+  import Footer from "./components/Header/Footer";
 
   export default {
     name: 'App',
     components: {
+      Footer,
       Header
     }
   }
@@ -23,9 +26,11 @@
 <style>
   #app {
     flex-direction: column;
+    min-height: 100%;
   }
   .page-container {
     margin: 0 5%;
     justify-content: center;
+    flex: 1;
   }
 </style>
---
+++ front/src/components/Header/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="footer">
+        <div></div>
+        <div class="contact">
+            {{$t('footer.contact')}}{{nbsp}}:{{nbsp}}<a href = "mailto: admin@rouzig.bzh">admin@rouzig.bzh</a>
+        </div>
+        <div class="copyright">
+            &copy;Rouzig 2020
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
