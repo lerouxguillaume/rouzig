94831843d547a1019287c408d3ba4fcf469884bd
Guillaume Le Roux <guillaume.le.roux@hotmail.fr>
2020-05-28 23:07:18 +0100

    handle transition

--- front/src/components/Form/TranslationForm.vue
+++ front/src/components/Form/TranslationForm.vue
@@ -3,91 +3,24 @@
         <div class="row">
             <PanelHeader v-model="page"></PanelHeader>
         </div>
-        <div class="translation-form-container"  v-show="page === 1">
-            <div class="row">
-                <TextInput
-                        id="word-original-input"
-                        class-name="col-4"
-                        :label="$t('form.word-name.label')"
-                        v-model="translation.originalWord.text"
-                        :placeholder="$t('form.word-name.placeholder')"
-                        :error="$t(translation.originalWord.wordError)"
-                        :readonly="readonly"
-                >
-                </TextInput>
-                <LanguageSelection
-                        id="language-selection"
-                        :origin="translation.originalWord.language"
-                        :destination="translation.translatedWord.language"
-                        @update:origin="value => translation.originalWord.language = value"
-                        @update:destination="value => translation.translatedWord.language = value"
-                        :readonly="readonly"
-                ></LanguageSelection>
-                <TextInput
-                        id="word-translated-input"
-                        class-name="col-4"
-                        :label="$t('form.word-name.label')"
-                        v-model="translation.translatedWord.text"
-                        :placehTextAreaInputolder="$t('form.word-name.placeholder')"
-                        :error="$t(translation.originalWord.wordError)"
-                        :readonly="readonly"
-                >
-                </TextInput>
-            </div>
-            <div class="row">
-                <TextAreaInput
-                        id="translation-description"
-                        class-name="col-12"
-                        :label="$t('form.word-name.label')"
-                        :value="translation.translatedWord.description"
-                        :readonly="readonly"
-                >
-
-                </TextAreaInput>
-            </div>
-            <div class="row">
-                <SelectInput
-                        id="option-input"
-                        class-name="col-12"
-                        :label="$t('form.word-type.label')"
-                        v-model="translation.originalWord.wordType"
-                        :options="typeOptions"
-                        :error="$t(translation.originalWord.typeError)"
-                        :readonly="readonly"
-                >
-                </SelectInput>
-            </div>
-            <!--            <SelectInput-->
-            <!--                    id="option-input"-->
-            <!--                    :label="$t('form.word-type.label')"-->
-            <!--                    v-model="translation.originalWord.wordType"-->
-            <!--                    :options="typeOptions"-->
-            <!--                    :error="$t(translation.originalWord.typeError)"-->
-            <!--                    :readonly="readonly"-->
-            <!--            ></SelectInput>-->
-            <!--        <WordForm :word="translation.originalWord"></WordForm>-->
-            <!--        <WordForm :word="translation.translatedWord"></WordForm>-->
-            <slot name="submit"></slot>
+        <div class="grow">
+            <transition name="component-fade" mode="out-in">
+                <component v-bind:is="currentComponent" v-bind="props"></component>
+            </transition>
         </div>
-        <div v-show="page === 2">
-            <ExamplesForm :examples="translation.examples" :readonly="readonly"></ExamplesForm>
-        </div>
+        <slot name="submit"></slot>
     </b-form>
 </template>
 (
 <script>
     import ExamplesForm from "./ExamplesForm";
     import {Translation} from "../../entities/Translation";
-    import TextInput from "./TextInput";
-    import SelectInput from "./SelectInput";
-    import {Languages, WordTypes} from "../../utils/enum";
-    import LanguageSelection from "./LanguageSelection";
-    import TextAreaInput from "./TextAreaInput";
     import PanelHeader from "../Utils/PanelHeader";
+    import WordForm from "./WordForm";
     export default {
         name: "TranslationForm",
         // eslint-disable-next-line vue/no-unused-components
-        components: {PanelHeader, TextAreaInput, LanguageSelection, SelectInput, TextInput, ExamplesForm},
+        components: {WordForm, PanelHeader, ExamplesForm},
         props: {
             translation: {
                 required: true,
@@ -98,30 +31,44 @@
                 default: false
             }
         },
-        data: () => {
+        data () {
             return {
                 page: 1,
+                components: [
+                    {
+                        key: WordForm,
+                        props: {
+                            translation: this.translation,
+                            readonly: this.readonly
+                        }
+                    },
+                    {
+                        key: ExamplesForm,
+                        props: {
+                            examples: this.translation.examples,
+                            readonly: this.readonly
+                        }
+                    },
+                ]
             }
         },
-        computed : {
-            languageOptions() {
-                return Languages();
+        computed: {
+            currentComponent() {
+                return this.components[this.page - 1].key;
             },
-            typeOptions() {
-                return WordTypes();
-            }
+            props() {
+                return this.components[this.page - 1].props;
+            },
         }
     }
 </script>
 
 <style scoped>
-    .row {
-        align-items: flex-end;
-        justify-content: space-between;
+    .component-fade-enter-active, .component-fade-leave-active {
+        transition: opacity .3s ease;
     }
-    .translation-form-container {
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
+    .component-fade-enter, .component-fade-leave-to
+        /* .component-fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
     }
 </style>
--- front/src/components/Form/WordForm.vue
+++ front/src/components/Form/WordForm.vue
@@ -1,54 +1,84 @@
 <template>
-    <b-card
-
-            header-bg-variant="primary"
-            border-variant="primary"
-            :header="header"
-    >
+    <div class="word-form-container">
         <div class="row">
             <TextInput
-                    id="text-input"
+                    id="word-original-input"
+                    class-name="col-4"
                     :label="$t('form.word-name.label')"
-                    v-model="word.text"
+                    v-model="translation.originalWord.text"
                     :placeholder="$t('form.word-name.placeholder')"
-                    :error="$t(word.wordError)"
+                    :error="$t(translation.originalWord.wordError)"
+                    :readonly="readonly"
             >
             </TextInput>
-            <SelectInput
-                    id="option-input"
-                    :label="$t('form.word-type.label')"
-                    v-model="word.wordType"
-                    :options="typeOptions"
-                    :error="$t(word.typeError)"
-            ></SelectInput>
-            <SelectInput
-                    id="option-input"
-                    :label="$t('form.word-language.label')"
-                    v-model="word.language"
-                    :options="languageOptions"
-                    :error="$t(word.languageError)"
-            ></SelectInput>
+            <LanguageSelection
+                    id="language-selection"
+                    :origin="translation.originalWord.language"
+                    :destination="translation.translatedWord.language"
+                    @update:origin="value => translation.originalWord.language = value"
+                    @update:destination="value => translation.translatedWord.language = value"
+                    :readonly="readonly"
+            ></LanguageSelection>
+            <TextInput
+                    id="word-translated-input"
+                    class-name="col-4"
+                    :label="$t('form.word-name.label')"
+                    v-model="translation.translatedWord.text"
+                    :placehTextAreaInputolder="$t('form.word-name.placeholder')"
+                    :error="$t(translation.originalWord.wordError)"
+                    :readonly="readonly"
+            >
+            </TextInput>
         </div>
-    </b-card>
+        <div class="row">
+            <TextAreaInput
+                    id="translation-description"
+                    class-name="col-12"
+                    :label="$t('form.word-name.label')"
+                    :value="translation.translatedWord.description"
+                    :readonly="readonly"
+            >
+
+            </TextAreaInput>
+        </div>
+        <div class="row">
+            <SelectInput
+                    id="option-input"
+                    class-name="col-12"
+                    :label="$t('form.word-type.label')"
+                    v-model="translation.originalWord.wordType"
+                    :options="typeOptions"
+                    :error="$t(translation.originalWord.typeError)"
+                    :readonly="readonly"
+            >
+            </SelectInput>
+        </div>
+    </div>
 </template>
 
 <script>
     import {Languages, WordTypes} from "../../utils/enum";
-    import {Word} from "../../entities/Word";
     import SelectInput from "./SelectInput";
     import TextInput from "./TextInput";
+    import {Translation} from "../../entities/Translation";
+    import TextAreaInput from "./TextAreaInput";
+    import LanguageSelection from "./LanguageSelection";
 
     export default {
         name: "WordForm",
-        components: {TextInput, SelectInput},
+        components: {LanguageSelection, TextAreaInput, TextInput, SelectInput},
         props: {
-            word: {
-                type: Word,
+            translation: {
+                type: Translation,
                 required: true
             },
             header: {
                 type: String,
                 required: false,
+            },
+            readonly: {
+                type: Boolean,
+                default: false
             }
         },
         computed : {
@@ -63,13 +93,13 @@
 </script>
 
 <style scoped>
-    .translation-container {
+    .word-form-container {
+        display: flex;
         flex-direction: column;
         flex-grow: 1;
     }
-    .delete-button-container {
-        margin-top: 20px;
-        display: flex;
+    .row {
+        align-items: flex-end;
         justify-content: space-between;
     }
 </style>
